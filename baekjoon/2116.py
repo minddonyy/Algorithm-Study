fbce45f7d3b046a4e1a6ec2818836090245e5433
minddonyy <minddonyy@gmail.com>
2025-02-28 08:06:06 +0900

    0228 update

--- baekjoon/2116.py
+++ baekjoon/2116.py
@@ -7,15 +7,39 @@ sys.stdin = open('input_2116.txt', 'r')
 4. 1~6 일 때 sum값을 저장하고
 5. 거기서 가장 큰 sum값을 print
 """
+def find_bottom(top, dice):
+    pairs = {0: 5, 1: 3, 2: 4, 3: 1, 4: 2, 5: 0}
+    top_index = dice.index(top)  #
+    bottom_index = pairs[top_index]
+    return dice[bottom_index]
 
+def find_side():
+    pass
 N = int(input())
-matrix = [list(map(int,input().split())) for _ in range(N)]
+dice = [list(map(int,input().split())) for _ in range(N)]
+max_value = 0
+result = 0
 
-sum = 0
-for i in range(1,7):
-    up_number = i # 첫번째 주사위의 윗면을 정함
-    for j in range(N):
-        pass
+for i in range(6):
+    top = dice[0][i]
+    bottom_value = find_bottom(top, dice[0])
+    total_sum = 0
+    side_value = max([x for x in dice[0] if x not in (top, bottom_value)])
+    total_sum = side_value
+    for j in range(1, N):
+        bottom_value = top
+        top = find_bottom(bottom_value, dice[j])
+        side_value = max([x for x in dice[j] if x not in (top, bottom_value)])
+        total_sum+=side_value
+    result = max(result, total_sum)
+
+print(result)
+
+
+
+
+
+
 
 
 
---
+++ baekjoon/2635.py
@@ -0,0 +1,5 @@
+import sys
+sys.stdin = open('input_2116.txt', 'r')
+########################################
+
+N = int(input())
